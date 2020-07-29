77205eb27e959d04c386fea0e4c6ef6e7985af25
abenevaut <abenevaut@users.noreply.github.com>
2020-07-29 08:37:38 +0000

    Update master to output generated at 9547584

--- phpmig.php
+++ phpmig.php
@@ -1,8 +1,14 @@
 <?php
 
-foreach (array(__DIR__ . '/../../../bedrock/vendor/autoload.php', __DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+$autoloads = array(
+    __DIR__ . '/../../../bedrock/vendor/autoload.php',
+    __DIR__ . '/../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/vendor/autoload.php'
+);
+
+foreach ($autoloads as $file) {
     if (file_exists($file)) {
         return require_once(__DIR__ . '/container.php');
     }
 }
-
